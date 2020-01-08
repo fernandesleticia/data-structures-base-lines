0ca229f0539fe1bda267ea0aa7c822115fafccc6
fernandesleticia <leticiafernandes777@gmail.com>
2020-01-07 23:19:27 -0300

    adding linked list

---
+++ linked-lists.rb
@@ -0,0 +1,21 @@
+''' Linked Lists is a data structure where each element is 
+a node containing two pieces of information (for a single linked list)→ 
+1) its data 
+2) a reference to the next piece of data in the list. 
+For a doubly linked list it holds 3 pieces of information → 
+1) a reference to the previous piece of data in the list 
+2) its data 
+3) a reference to the next piece of data in the list. 
+The entry point to the linked list is the head, 
+and the only way to get through the list is by iterating 
+until you find the item you want'''
+
+class Node    
+    attr_accessor :value, :next
+
+    def initialize(value, next_node)
+        @value = value
+        @next = next_node
+    end
+end
+
