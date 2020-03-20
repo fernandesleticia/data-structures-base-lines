6931d50a248727f66286d2ec161658c13a1de601
fernandesleticia <leticiafernandes777@gmail.com>
2020-03-20 15:09:09 -0300

    add rebalance for add

--- heap.rb
+++ heap.rb
@@ -56,29 +56,37 @@ def after_pop_adjust
   if size > 1
     @size -= 1
     @heap[0] = @heap[@size]
-    #rebalance
+    rebalance_down(0)
   else
     @size = 0 
   end 
 end
 
-def after_pop_adjust
+def after_add_adjust
   @size += 1
-  #rebalance
+  rebalance_up(size - 1)
 end
 
 def rebalance_down(i)
   left_i = left(i)
   right_i = right(i)
 
- if has_left(i) and compare(@heap[left_i], @heap[i]) and (not has_right(i) or compare(@heap[left_i], @heap[right_i]))
-      @heap[i], @heap[left_i] = @heap[left_i], @heap[i]
-      rebalance_down(left_i)
- elsif has_right(i) and compare(@heap[right_i], @heap[i])
-      @heap[i], @heap[right_i] = @heap[right_i], @heap[i]
-      rebalance_down(right_i)
- end 
+  if has_left(i) and compare(@heap[left_i], @heap[i]) and (not has_right(i) or compare(@heap[left_i], @heap[right_i]))
+    @heap[i], @heap[left_i] = @heap[left_i], @heap[i]
+    rebalance_down(left_i)
+  elsif has_right(i) and compare(@heap[right_i], @heap[i])
+    @heap[i], @heap[right_i] = @heap[right_i], @heap[i]
+    rebalance_down(right_i)
+  end 
+end
 
+def rebalance_up(i)
+  parent_i = parent(i)
+
+  if has_parent(i) and compare(@heap[i], @heap[parent_i])
+    @heap[i], @heap[parent_i] = @heap[parent_i], @heap[i]
+    rebalance_up(parent_i)
+  end
 end
 
 def left(index)
@@ -95,4 +103,12 @@ end
 
 def has_right(index)
   right(index) < size
+end
+
+def has_parent(i)
+  i >= 1
+end
+
+def parent(i)
+  ((i - 1) / 2).floor
 end
