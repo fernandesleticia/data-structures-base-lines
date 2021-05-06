f5dcde5be3fbd771655bcd7aad635f5ecfc1af9f
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-05-06 19:38:31 -0300

    adding height of tree

--- tree_solutions/height_of_a_binary_tree.rb
+++ tree_solutions/height_of_a_binary_tree.rb
@@ -1,19 +1,15 @@
 #number of edges between the tree's root and its furthest leaf
 
-tree = [5, [2,3]]
-
+tree = [5, [2, [5], [4]], [3]]
 def height(tree)
     return 0 unless tree.length > 1
-
-
+    get_height(tree) 
 end
 
-def get_height(tree, count_left=0, count_right=0)
-    count_left+=1
-    count_right+=1
+def get_height(tree)
+    return 0 if tree.nil?
     
-    get_height(tree[1][0])
-    get_height(tree[1][1])
+    [get_height(tree[1]), get_height(tree[2])].max + 1
 end
 
 p height(tree)
